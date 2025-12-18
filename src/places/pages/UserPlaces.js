9fc514354964761d0acc93c16447a078c7d5f83b
yashGist <ramawatyashdeep@gmail.com>
2025-12-18 12:37:33 +0000

    all working

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -2,6 +2,6 @@ import React from "react";
 import "./UserPlaces.css";
 
 const UserPlaces = () => {
-  return <div className="user-places">User Places Page</div>;
+  return <div className="user-places"> User Places Page</div>;
 };
 export default UserPlaces;
