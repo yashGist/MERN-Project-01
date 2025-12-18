075c860513a17051f743962fac0524eb6e87362b
yashGist <ramawatyashdeep@gmail.com>
2025-12-18 14:26:46 +0000

    cards overlapping issue fixed. working

--- src/user/components/UIElements/Card.js
+++ src/user/components/UIElements/Card.js
@@ -1,10 +1,9 @@
 import React from "react";
-
 import "./Card.css";
 
 const Card = (props) => {
   return (
-    <div className={`card ${props.className}`} style={props.style}>
+    <div className={`card ${props.className || ""}`} style={props.style}>
       {props.children}
     </div>
   );
