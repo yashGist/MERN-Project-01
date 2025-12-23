e6ed946d44649fc34ce11d3edf579908fff86d87
yashGist <ramawatyashdeep@gmail.com>
2025-12-23 08:19:56 +0000

    LOGOUT button added

--- src/user/components/Navigation/NavLinks.js
+++ src/user/components/Navigation/NavLinks.js
@@ -24,6 +24,11 @@ const NavLinks = (props) => {
           <NavLink to="/auth"> AUTHENTICATE</NavLink>
         </li>
       )}
+      {auth.isLoggedIn && (
+        <li>
+          <button onClick={auth.logout}>LOGOUT</button>
+        </li>
+      )}
     </ul>
   );
 };
