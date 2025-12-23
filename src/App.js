91636bd0e5563a60cf7c83ab14ed217ffdefc5a0
yashGist <ramawatyashdeep@gmail.com>
2025-12-23 06:21:18 +0000

    Auth logic added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./user/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 const App = () => {
   return (
     <Router>
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="/places/:placeId/edit" element={<UpdatePlace />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </main>
     </Router>
---
+++ src/user/pages/Auth.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Input from "../../shared/FormElements/Input";
+import "../../shared/FormElements/Input.css";
+import "../pages/Auth.css";
+import Button from "../../shared/FormElements/Button";
+import Card from "../components/UIElements/Card";
+import { useForm } from "../../shared/hooks/form-hook";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_EMAIL,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+
+const Authenticate = () => {
+  const [isLoginMode, setIsLoginMode] = useState(true);
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      email: { value: "", isValid: false },
+      password: { value: "", isValid: false },
+    },
+    false
+  );
+
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          email: formState.inputs.email,
+          password: formState.inputs.password,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: { value: "", isValid: false },
+        },
+        false
+      );
+    }
+    setIsLoginMode((prev) => !prev);
+  };
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  return (
+    <Card className="authentication">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {!isLoginMode && (
+          <Input
+            id="name"
+            element="input"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please Enter a Name"
+            onInput={inputHandler}
+          />
+        )}
+
+        <Input
+          id="email"
+          element="input"
+          type="email"
+          label="Email"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address."
+          onInput={inputHandler}
+        />
+
+        <Input
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a password with at least 5 characters"
+          onInput={inputHandler}
+        />
+
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoginMode ? "LOGIN" : "SIGN-UP"}
+        </Button>
+      </form>
+
+      <Button inverse onClick={switchModeHandler}>
+        SWITCH TO {isLoginMode ? "SIGN-UP" : "LOGIN"}
+      </Button>
+    </Card>
+  );
+};
+
+export default Authenticate;
